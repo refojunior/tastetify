102825a8e2bcebdda397281d104fd7fbf6abf7e1
styles <refojunior5@gmail.com>
2021-08-01 00:33:52 +0800

    meta tag

--- resources/js/Components/Banner.vue
+++ resources/js/Components/Banner.vue
@@ -5,7 +5,7 @@
                 <div class="row flex-lg align-items-center g-5 py-5">                    
                     <div class="col-lg-12 text-center">
                         <h1 class="display-5 fw-bold lh-1 mb-3">Tastetify</h1>
-                        <p class="lead">Create a cool card with your taste of music!</p>
+                        <p class="lead">Generate a cool card with several templates or get to know your most played songs or artists in Spotify!</p>
                         <div class="d-grid gap-2 d-md-flex justify-content-center">
                             <a v-if="user.length == 0" href="/login" class="btn btn-success px-4 me-md-2">Create your own</a>
                             <inertia-link v-else href="/generator" class="btn btn-success px-4 me-md-2">Create your own</inertia-link>
@@ -28,7 +28,16 @@ export default {
 }
 </script>
 <style scoped>
-    
+    .lead {
+        max-width: 620px;
+        margin: auto;
+        margin-bottom: 20px;
+        text-shadow:
+        -1px -1px 0 #fff,  
+            1px -1px 0 #fff,
+            -1px 1px 0 #fff,
+            1px 1px 0 #fff;
+    }
     .banner {
         background-image: url('/img/banner.jpg');
         background-size: cover;
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
-    <title>{{ $page['props']['title'] ?? 'Hello World' }}</title>
+    <title>{{ $page['props']['title'] ?? 'Hello World' }} - Tastetify</title>
     <script src="{{ mix('/js/app.js') }}" defer></script>
+
+    <!-- GOOGLE META-->
+    <meta name="robots" content="index,follow">
+    <meta name="description" content="Generate a cool card with several templates or get to know your most played songs or artists in Spotify! Visit Tastetify now." />
+
+    <!-- TWITTER META -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@tastetify">
+    <meta name="twitter:creator" content="@styles77">
+    <meta name="twitter:title" content="Welcome - Tastetify">
+    <meta name="twitter:description" content="Generate a cool card with several templates or get to know your most played songs or artists in Spotify! Visit Tastetify now.">
+    <meta name="twitter:image" content="https://tastetify.herokuapp.com/img/banner-meta.jpg">
+
+    <!-- FACEBOOK META -->
+    <meta property="og:url" content="https://tastetify.herokuapp.com" />
+    <meta property="og:type" content="website" />
+    <meta property="og:title" content="Welcome - Tastetify" />
+    <meta property="og:description" content="Generate a cool card with several templates or get to know your most played songs or artists in Spotify! Visit Tastetify now." />
+    <meta property="og:image" content="https://tastetify.herokuapp.com/img/banner-meta.jpg" />
   
   </head>
   <body>
