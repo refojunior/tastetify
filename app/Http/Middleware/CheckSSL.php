e7db6426ae4857e9d5cd37050d695d313907febf
styles <refojunior5@gmail.com>
2021-08-01 15:22:24 +0800

    fix

--- app/Http/Middleware/CheckSSL.php
+++ app/Http/Middleware/CheckSSL.php
@@ -16,11 +16,15 @@ class CheckSSL
      */
     public function handle(Request $request, Closure $next)
     {
-        if(env('APP_ENV', 'local') != 'local'){            
-            if (strpos(request()->fullUrl(), 'https://') != 0) {                
-                return redirect(env('APP_URL').$request->path());
-            } else {
+        if(env('APP_ENV', 'local') != 'local'){    
+            if (isset($_SERVER['HTTPS']) &&
+                ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+                isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+                $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https') {
                 return $next($request);
+            }
+            else {
+                redirect(env('APP_URL').$request->path());
             }
         } else {
             return $next($request);
